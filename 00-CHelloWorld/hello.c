56917a50f908af0995d23203d1ecf03996ad5d5f
Nico <nimustafa03@gmail.com>
2026-04-08 00:11:47 -0300

    Trabajo terminado.

---
+++ 00-CHelloWorld/hello.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(void){
+    fputs("Hola, mundo.",stdout);
+    return 0;
+}
---
+++ 00-CHelloWorld/readme.md
@@ -0,0 +1,3 @@
+Compilador seleccionado: Clang
+Version: 22.1.2
+Versión de C que compila: C23
